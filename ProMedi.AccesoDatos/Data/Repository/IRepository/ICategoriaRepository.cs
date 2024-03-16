b34b7a023cfa25edd37202f61a219aa3b74d9712
Maria Candela Martinez <80454271+CandelaMartinez@users.noreply.github.com>
2024-03-16 16:35:32 +0100

    implementacion de IRepositoryPattern

---
+++ ProMedi.AccesoDatos/Data/Repository/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProMedi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProMedi.AccesoDatos.Data.Repository.IRepository
+{
+    public interface ICategoriaRepository : IRepository<Categoria>
+    {
+        void Update(Categoria categoria);
+        //IEnumerable<SelectListItem> GetListaCategorias();
+
+    }
+}
