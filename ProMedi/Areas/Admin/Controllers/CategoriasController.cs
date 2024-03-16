08a0cdc55a9633099032eb56b61d1c465655b939
Maria Candela Martinez <80454271+CandelaMartinez@users.noreply.github.com>
2024-03-16 17:32:04 +0100

    controlador categorias y js necesario para usar datatable

---
+++ ProMedi/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProMedi.AccesoDatos.Data.Repository.IRepository;
+
+namespace ProMedi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoriasController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        //muestra una vista
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //POST: trae informacion de un formulario
+        #region Llamadas a la API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new {data = _unitOfWork.Categoria.GetAll()});
+        }
+        #endregion
+    }
+}
