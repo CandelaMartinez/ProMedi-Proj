27b1f2698cd3dd7710494fa43c123c6d95bb5996
Maria Candela Martinez <80454271+CandelaMartinez@users.noreply.github.com>
2024-03-23 15:54:17 +0100

    migraciones

---
+++ ProMedi.AccesoDatos/Migrations/20240323145024_creacionTablaPublicacion.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProMedi.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class creacionTablaPublicacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Publicaciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaCreacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlImagen = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publicaciones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Publicaciones_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publicaciones_CategoriaId",
+                table: "Publicaciones",
+                column: "CategoriaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Publicaciones");
+        }
+    }
+}
