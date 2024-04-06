a8632b5162ba042c3fb858fde02bf321f1af91d6
Maria Candela Martinez <80454271+CandelaMartinez@users.noreply.github.com>
2024-04-06 12:58:23 +0200

    creo CarrouselesControler con metodo y vista para Index

---
+++ ProMedi/Areas/Admin/Controllers/CarrouselesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProMedi.AccesoDatos.Data.Repository.IRepository;
+
+namespace ProMedi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CarrouselesController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CarrouselesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        //muestra una vista
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
