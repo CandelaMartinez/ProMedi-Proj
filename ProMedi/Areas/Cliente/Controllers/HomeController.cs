501c6149bd756d8bd8c835e8332b7920b64c7e06
Maria Candela Martinez <80454271+CandelaMartinez@users.noreply.github.com>
2024-04-13 13:36:20 +0200

    metodos en el controlador Home

--- ProMedi/Areas/Cliente/Controllers/HomeController.cs
+++ ProMedi/Areas/Cliente/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using ProMedi.AccesoDatos.Data.Repository.IRepository;
 using ProMedi.Models;
+using ProMedi.Models.ViewModels;
 using System.Diagnostics;
 
 namespace ProMedi.Areas.Cliente.Controllers
@@ -7,17 +9,39 @@ namespace ProMedi.Areas.Cliente.Controllers
     [Area("Cliente")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        //obtengo los datos desde el contenedor de trabajo que encapsula los repositorios
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            _unitOfWork = unitOfWork;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            //instancio el viewModel que le pasare a la vista 
+            HomeViewModel homeVM = new HomeViewModel()
+            {
+                Carrouseles = _unitOfWork.Carrousel.GetAll(),
+                Publicaciones = _unitOfWork.Publicacion.GetAll()
+            };
+
+            //envio esta variable para indicar que estamos en la pagina Home
+            //porque solo necesito mostrar el carrousel en la pagina Home
+            //y como puse el carrousel en el layout principal, necesito especificarlo
+            ViewBag.IsHome = true;
+
+            return View(homeVM);
         }
+
+        //metodo llamado desde home.cshtml para mostrar los detalles de la publicacion en la card
+        //recibe el id de la publicacion a buscar 
+        [HttpGet]
+        public IActionResult Details(int id) 
+        {
+            var publicacionDesdeDB = _unitOfWork.Publicacion.Get(id);
+            return View(publicacionDesdeDB);
+        }
+
 
         public IActionResult Privacy()
         {
