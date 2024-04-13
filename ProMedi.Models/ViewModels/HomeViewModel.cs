40d736794b1ea685fb91a31dace690cc3cc55572
Maria Candela Martinez <80454271+CandelaMartinez@users.noreply.github.com>
2024-04-13 13:34:34 +0200

    Viewmodel necesario para poder acceder a las publicaciones y los carrouseles

---
+++ ProMedi.Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProMedi.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        //necesito crear un viewModel porque le paso a la vista data de dos modelos diferentes
+        public IEnumerable<Carrousel> Carrouseles { get; set;}
+        public IEnumerable<Publicacion> Publicaciones { get; set;}
+
+    }
+}
