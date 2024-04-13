dd8c16464ed687b04363f18ca59f8749a8d05ad6
Maria Candela Martinez <80454271+CandelaMartinez@users.noreply.github.com>
2024-04-13 19:05:00 +0200

    creo constantes con los roles

---
+++ ProMedi.Utilidades/Constantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProMedi.Utilidades
+{
+    public class Constantes
+    {
+        public const string Administrador = "Admin";
+        public const string Medico = "Medico";
+
+    }
+}
